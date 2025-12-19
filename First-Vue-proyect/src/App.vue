8542f569138d69ae846292ec86a0103aa9c6e600
Chimochuchis02 <angeldominguezlopez030206@gmail.com>
2025-12-19 13:56:00 -0600

    First Vue Poroyect, v-for and v-on

---
+++ First-Vue-proyect/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+  const Cards = 
+  [{id: 1, texto:"Clases Practicas"},
+   {id: 2, texto:"Material Didactico"},
+   {id: 3, texto:"Clases Virtuales y presenciales"},
+   {id: 4, texto:"Tarjeta 4"},
+   {id: 5, texto:"Tarjeta 5"}
+  ]
+  const salutate = (texto) => {
+    alert("Entrando a: " + texto)
+  }
+
+</script>
+
+<template>
+  <div class="card-container">
+  <div v-for="card in Cards" :key=card.id class="cards" @click="salutate(card.texto)">
+  {{ card.texto }}
+  </div>
+  </div>
+  
+  
+
+  <main>
+    <p>!!!Estoy estudiando para 5to cuatrimestre!!!.</p>
+  </main>
+</template>
+
+<style scoped>
+  .cards{
+    background-color: white;
+    color: black;
+    margin: 10px;
+    padding: 20px;
+    border-radius: 8px;
+    
+  }
+</style>
