0a985d5aef2067de13856de757dbcb7d72072a9c
Chimochuchis02 <angeldominguezlopez030206@gmail.com>
2025-12-21 22:23:28 -0600

    My First Component in VUE.

--- First-Vue-proyect/src/App.vue
+++ First-Vue-proyect/src/App.vue
@@ -1,38 +1,33 @@
 <script setup>
-  const Cards = 
-  [{id: 1, texto:"Clases Practicas"},
-   {id: 2, texto:"Material Didactico"},
-   {id: 3, texto:"Clases Virtuales y presenciales"},
-   {id: 4, texto:"Tarjeta 4"},
-   {id: 5, texto:"Tarjeta 5"}
-  ]
-  const salutate = (texto) => {
+import tarjeta from "./components/Tarjetas.vue"
+const salutate = (texto) => {
     alert("Entrando a: " + texto)
   }
-
+  const Cards = [
+  { id: 1, texto: "Clases Practicas" },
+  { id: 2, texto: "Material Didactico" },
+  { id: 3, texto: "Clases Virtuales" },
+  { id: 4, texto: "Tarjeta 4" },
+  { id: 5, texto: "Tarjeta 5" }
+]  
 </script>
 
 <template>
   <div class="card-container">
-  <div v-for="card in Cards" :key=card.id class="cards" @click="salutate(card.texto)">
-  {{ card.texto }}
+  <tarjeta v-for="card in Cards" :key=card.id :text=card.texto @clickBoton="salutate(card.texto)"
+  />
   </div>
-  </div>
-  
-  
-
-  <main>
-    <p>!!!Estoy estudiando para 5to cuatrimestre!!!.</p>
-  </main>
 </template>
 
 <style scoped>
-  .cards{
-    background-color: white;
-    color: black;
-    margin: 10px;
-    padding: 20px;
-    border-radius: 8px;
-    
-  }
+body{
+  margin: 0;
+  padding: 0;
+  background: radial-gradient(circle, #3b3b3b 0%, #000000 100%);
+  min-height: 100vh;
+} 
+
+.card-container{
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
 </style>
---
+++ First-Vue-proyect/src/components/Tarjetas.vue
@@ -0,0 +1,37 @@
+<script setup>
+// Defino qué datos necesita recibir esta tarjeta para funcionar
+defineProps(['text']) 
+</script>
+
+<template>
+ <div class="card-V2" @click="$emit('clickBoton')">
+ <h3>{{ text }}</h3>
+ 
+</div>
+</template>
+
+<style scoped>
+  .card-V2{
+  background-color: white;
+  color: #333;
+  padding: 20px;
+  border-radius: 12px;
+  border-left: 5px solid #42b883; /* El verde de Vue */
+  margin: 10px;
+  box-shadow: 0 4px 6px rgba(0,0,0,0.1);
+  transition: transform 0.2s;
+  }
+  
+  .card-V2:hover{
+   transform: translateY(-5px);
+  }
+
+  button{
+  background: #42b883;
+  color: white;
+  border: none;
+  padding: 8px 15px;
+  border-radius: 5px;
+  cursor: pointer;
+  }
+</style>
