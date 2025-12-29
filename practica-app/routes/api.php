98be449eb87910c24c8bba47a96001c92766fef9
Chimochuchis02 <angeldominguezlopez030206@gmail.com>
2025-12-28 19:21:11 -0600

    Back && Front

---
+++ practica-app/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::get('/tarjetas', function () {
+    return DB::table('services')->get();
+});
